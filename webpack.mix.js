d79d4ed32f6c4d495ce61699e013c92e5639e53c
klejdis jorgji <klejdis.jorgji@y42.com>
2022-04-21 20:25:02 +0200

    struct of project

--- webpack.mix.js
+++
@@ -1,9 +0,0 @@
-const mix = require('laravel-mix');
-let path = require('path');
-let webpack = require('webpack');
-
-
-mix.ts('resources/js/app.ts', 'public/js/app.js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);;
