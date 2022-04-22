aaf9e6105b0a7fc2b10cb380440be9e95a8968f2
klejdis jorgji <klejdis.jorgji@y42.com>
2022-04-22 16:09:36 +0200

    forms module

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lara-angular';
+  username = '';
+
+  handleClick(value: string){
+    console.log(value);
+  }
+
+  handleInput(val: any){
+     this.username = val.target.value;
+  }
+
 }
