c3f5f753aaf326d161a0fa95cf228686713ce027
klejdis jorgji <klejdis.jorgji@y42.com>
2022-04-27 15:54:50 +0200

    wip

--- Client/src/app/modules/users/components/list/users.component.ts
+++ Client/src/app/modules/users/components/list/users.component.ts
@@ -1,4 +1,4 @@
-import { Component} from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'users-list',
@@ -6,6 +6,9 @@ import { Component} from "@angular/core";
   styleUrls: ['./users.component.scss']
 
 })
-export class UsersComponent{
+export class UsersComponent implements OnInit{
 
+  ngOnInit() {
+
+  }
 }
